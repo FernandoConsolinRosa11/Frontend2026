642a9b944950fcb50070f90a8653e1166a8616f6
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2026-02-23 15:31:01 -0300

    First Commit

---
+++ my-frontend2026/src/components/layout/carCarousel/carCarousel.tsx
@@ -0,0 +1,65 @@
+import carCardOne from "../../../assets/images/carCardOne.png";
+import carCardTwo from "../../../assets/images/carCardTwo.png";
+
+export const CarCarousel = () => {
+  return (
+    <>
+      <div id="carouselExampleIndicators" className="carousel slide max-w-4xs mt-0.5">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={carCardOne} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item active">
+            <img src={carCardTwo} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
+};
---
+++ my-frontend2026/src/components/layout/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="font-bold text-2xl bg-amber-50 shadow py-3">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
