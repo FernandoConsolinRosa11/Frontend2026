bc5fd76d38da984961a9f6246b9145d3ddc28856
AitomD <aitomdonatoni@gmail.com>
2026-03-04 19:55:34 -0300

    Router Dom
    adicionado o arquivo com as logicas de rotas do site para navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Routes } from 'react-router-dom';
 import './App.css'
-import Home from './pages/Home';
+import AppRoutes from './routes/AppRoutes';
 function App() {
 
   return (
-    <Home/>
-  )
+    <AppRoutes/>
+  );
 }
 
 export default App;
