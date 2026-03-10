eba0c15bff424303d3785550dfe623d38c44d175
AitomD <aitomdonatoni@gmail.com>
2026-03-10 20:11:57 -0300

    ajuste

--- src/pages/ErrorPage/Index.tsx
+++ src/pages/ErrorPage/Index.tsx
@@ -1,10 +1,5 @@
 export default function ErrorPage() {
-    return (
-        <>
-            <div>
-                <h1 className="text-white">Erro 404 - Página não encontrado</h1>
-            </div>
-
-        </>
-    );
+    <div>
+        
+    </div>
 }
