b4aeaa019a7b9a456a358f4f5b8eb07c8bc52fd7
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2026-03-20 09:43:36 -0300

    apiServicesInit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://http://localhost:5173",
+    baseURL: "http://localhost:3000",
     timeout: 5000, 
     headers: { "Content-Type": "application/json" },
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 export default api;
